09c994f2968fe2521822afec0e9534da2ad02075
DariusCapizzi <DariusCapizzi@gmail.com>
2016-04-01 09:05:13 -0700

    perform basic logic for ping-pong, and output that logic into a dynamic unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,43 @@
 $(function(){
 
+
+// ui
   $("form").submit(function(e){
     e.preventDefault();
+    $("ul").empty()
+    pingOrPong( $("input").val() );
 
-
-    Ping_or_Pong($("input").val());
-
+    //put pingpong output into ^^
   });
 
-  function Ping_or_Pong(input){
 
-    
+
+//busyness
+
+  function pingOrPong(number){
+
+    result = []
+
+  // basic logic, ..return array? yes sure
+    for( i = 1 ; i <= number ; i++ ){
+      if (i % 15 == 0){
+        result.push( "pingpong" )
+      } else if (i % 3 == 0) {
+        result.push( "ping" )
+      } else if (i % 5 == 0) {
+        result.push( "pong" )
+      } else { result.push(i) }
+    }
+
+
+  // for every li made, put a new results[i] in it, targets just the last li
+    for ( i =  0 ; i < result.length; i++){
+      $("ul").append('<li > </li>');
+      $("li:last").text(result[i])
+    }
+
+    //should ^^ be in business or ui logic?
+
 
 
   }
