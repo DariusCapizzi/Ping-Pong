fde806f95436fc574d130474a3d6d2f660f35704
DariusCapizzi <DariusCapizzi@gmail.com>
2016-04-01 10:43:10 -0700

    add recursive pingpong (tabletennis)

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,102 @@
-$(function(){
+$(function() {
 
 
-// ui
-  $("form").submit(function(e){
+  // ui
+  $("form").submit(function(e) {
     e.preventDefault();
-    $("ul").empty()
-    pingOrPong( $("input").val() );
 
-    //put pingpong output into ^^
+    //with iteration
+    // pingOrPong($("input").val());
+
+    //with recursion
+    tableTennis(1, $("input").val());
+
+
   });
 
 
 
-//busyness
+  //busyness
 
-  function pingOrPong(number){
+  function tableTennis(start, end){
+    //input test
+    $("h4").text("pingpong")
 
-    result = []
+    if (/\D/.test(end)) {
+      $("h4").text("hey, one number only")
+    }
 
-  // basic logic, ..return array? yes sure
-    for( i = 1 ; i <= number ; i++ ){
-      if (i % 15 == 0){
-        result.push( "pingpong" )
-      } else if (i % 3 == 0) {
-        result.push( "ping" )
-      } else if (i % 5 == 0) {
-        result.push( "pong" )
-      } else { result.push(i) }
+    if (start === 1){
+      result = []
     }
 
 
-  // for every li made, put a new results[i] in it, targets just the last li
-    for ( i =  0 ; i < result.length; i++){
+    if (start % 3 === 0 && start % 5 === 0 ){
+      result.push("pingpong")
+    } else if ( start % 3 === 0 ){
+      result.push("ping")
+    } else if (start % 5 ===0 ) {
+      result.push("pong")
+    } else {result.push(start);}
+
+    if (start < end){
+      tableTennis(start+1, end)
+    }
+
+
+    if (start == end - 1){
+      $("ul").empty()
+      for (i = 0; i < result.length; i++) {
+        $("ul").append('<li > </li>');
+        $("li:last").text(result[i])
+      }
+    }
+
+
+
+  }
+
+
+
+
+
+
+
+
+  function pingOrPong(number) {
+    //input test
+    $("h4").text("pingpong")
+
+    if (/\D/.test(number)) {
+      $("h4").text("hey, one number only")
+    }
+
+
+    result = [];
+
+    // basic iterative logic, ..return array? yes sure
+    for (i = 1; i <= number; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result.push("pingpong")
+      } else if (i % 3 == 0) {
+        result.push("ping")
+      } else if (i % 5 == 0) {
+        result.push("pong")
+      } else {
+        result.push(i)
+      }
+    }
+
+    //empty the list before we put stuff in
+    $("ul").empty()
+
+    // for every li made, put a new results[i] in it, targets just the last li
+    for (i = 0; i < result.length; i++) {
       $("ul").append('<li > </li>');
       $("li:last").text(result[i])
     }
 
-    //should ^^ be in business or ui logic?
-
-
+    //should ^^ be in business or ui logic? see  branch:  liOption
 
   }
 
